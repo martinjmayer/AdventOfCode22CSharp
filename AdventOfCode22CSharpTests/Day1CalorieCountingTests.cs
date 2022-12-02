fd3b3d0f7f66d5f6ac11b3a954ff609c03ac588d
martinjmayer <martin.john.mayer@gmail.com>
2022-12-02 01:47:50 +0000

    Day 1: Initial working solution

---
+++ AdventOfCode22CSharpTests/Day1CalorieCountingTests.cs
@@ -0,0 +1,190 @@
+﻿using System.Diagnostics;
+
+namespace AdventOfCode22CSharpTests;
+
+public class Day1CalorieCountingTests
+{
+    [TestCase("0", 0)]
+    [TestCase("1", 1)]
+    [TestCase("1000", 1000)]
+    [TestCase("010", 10)]
+    [TestCase("1000,129", 1129)]
+    [TestCase("5,6,7,8,9", 35)]
+    [TestCase("1,1,1,1,1", 5)]
+    [TestCase("23747,42,1,1,1", 23792)]
+    public void Elf_TotalCalories_ReturnsSumOfCaloriesOfAllItems(string data, int expectedTotalCalories)
+    {
+        var elf = new Elf(data.Split(',').Select(int.Parse).ToList());
+        
+        Assert.That(elf.TotalCalories, Is.EqualTo(expectedTotalCalories));
+    }
+    
+    [Test]
+    public void ElfDataParser_Parse_WhenInputIsEmpty_ReturnsNoElves()
+    {
+        var parser = new ElfDataParser();
+        var elves = parser.Parse(String.Empty);
+        
+        Assert.That(elves.Count(), Is.EqualTo(0));
+    }
+
+    [TestCase("1000\n300\n5000\n\n\n\n\n\n", 1)]
+    [TestCase("1000\n300\n5000\n\n3928\n2008\n4592\n\n3711\n1239", 3)]
+    [TestCase("1000\n300\n5000\n\n\n\n3928\n2008\n4592\n\n\n3711\n1239", 3)]
+    public void ElfDataParser_Parse_ReturnsNumberOfElvesInData(string data, int expectedNumberOfElves)
+    {
+        var parser = new ElfDataParser();
+        var elves = parser.Parse(data);
+
+        Assert.That(elves.Count(), Is.EqualTo(expectedNumberOfElves));
+    }
+    
+    [TestCase("1000\n300\n5000\n\n\n\n\n\n", 6300)]
+    [TestCase("1000\n300\n5000\n\n3928\n2008\n4592\n\n3711\n1239", 10528)]
+    [TestCase("1000\n300\n5000\n\n\n\n3928\n2008\n4592\n\n\n3711\n1239", 10528)]
+    public void ElfCalorieRanker_GetElfWithMostCalories_ReturnsHighestNumberOfCaloriesOfAnyElf(string data, int expectedHighestNumOfCalories)
+    {
+        var parser = new ElfDataParser();
+        var elves = parser.Parse(data);
+
+        var ranker = new ElfCalorieRanker();
+        var topElf = ranker.GetElfWithMostCalories(elves);
+
+        Assert.That(topElf?.TotalCalories ?? 0, Is.EqualTo(expectedHighestNumOfCalories));
+    }
+
+    [TestCase("1\n1\n1\n1\n\n1\n1\n1\n\n1\n1\n\n1", 9)]
+    [TestCase("7\n7\n7\n\n5\n5\n5\n\n3\n3\n3\n\n1\n1\n1", 45)]
+    public void ElfCalorieRanker_GetTotalCaloriesOfTopElvesByMostCalories_With3Elves_ReturnsTop3ElvesCombinedCalories(string data, int expectedCombinedCalories)
+    {
+        var parser = new ElfDataParser();
+        var elves = parser.Parse(data);
+
+        var ranker = new ElfCalorieRanker();
+        var combinedCalories = ranker.GetTotalCaloriesOfTopElvesByMostCalories(elves, 3);
+
+        Assert.That(combinedCalories, Is.EqualTo(expectedCombinedCalories));
+    }
+    
+    // File Tests
+
+    [Test]
+    public void ElfCalorieRanker_GetElfWithMostCalories_Input1File_ReturnsAResult()
+    {
+        var data = File.ReadAllText("Input1.txt");
+        
+        var parser = new ElfDataParser();
+        var elves = parser.Parse(data);
+        
+        var ranker = new ElfCalorieRanker();
+        var topElf = ranker.GetElfWithMostCalories(elves);
+        
+        var actualTotalCalories = topElf?.TotalCalories ?? 0;
+        
+        Debug.WriteLine(actualTotalCalories);
+        Assert.That(actualTotalCalories, Is.GreaterThan(0));
+    }
+    
+    [Test]
+    public void ElfCalorieRanker_GetTotalCaloriesOfTopElvesByMostCalories_Input1FileWith3Elves_ReturnsAResult()
+    {
+        var data = File.ReadAllText("Input1.txt");
+        
+        var parser = new ElfDataParser();
+        var elves = parser.Parse(data);
+
+        var ranker = new ElfCalorieRanker();
+        var combinedCalories = ranker.GetTotalCaloriesOfTopElvesByMostCalories(elves, 3);
+
+        Assert.That(combinedCalories, Is.GreaterThan(0));
+    }
+}
+
+public class ElfCalorieRanker
+{
+    public Elf? GetElfWithMostCalories(IEnumerable<Elf?> elves)
+    {
+        return elves.MaxBy(elf => elf?.TotalCalories ?? 0);
+    }
+
+    private IEnumerable<Elf?> GetTopElvesByMostCalories(IEnumerable<Elf?> elves, int numberOfElves)
+    {
+        return elves.OrderByDescending(elf => elf?.TotalCalories ?? 0).Take(numberOfElves);
+    }
+    
+    public int GetTotalCaloriesOfTopElvesByMostCalories(IEnumerable<Elf?> elves, int numberOfElves)
+    {
+        return GetTopElvesByMostCalories(elves, numberOfElves).Select(elf => elf?.TotalCalories ?? 0).Sum();
+    }
+}
+
+public class ElfDataParser
+{
+    public IEnumerable<Elf?> Parse(string data)
+    {
+        var dataWithoutCarriageReturns = data.Replace("\r", string.Empty);
+
+        var lines = dataWithoutCarriageReturns.Split("\n");
+
+        var elves = new List<Elf?>();
+
+        Elf? currentElf = null;
+        
+        foreach (var line in lines)
+        {
+            currentElf ??= GenerateNewElf();
+
+            if (line == string.Empty)
+            {
+                if (HasCalorieLines(currentElf))
+                {
+                    elves.Add(currentElf);
+                }
+                
+                currentElf = null;
+                continue;
+            }
+
+            if (currentElf is not null && int.TryParse(line, out var lineInt))
+            {
+                currentElf.AddCalorieLine(lineInt);
+            }
+        }
+        
+        if (HasCalorieLines(currentElf))
+        {
+            elves.Add(currentElf);
+        }
+
+        return elves;
+    }
+
+    private static bool HasCalorieLines(Elf? elf)
+    {
+        return elf is not null && elf.NumberOfCalorieLines > 0;
+    }
+
+    private static Elf? GenerateNewElf()
+    {
+        return new Elf(new List<int>());
+    }
+}
+
+public class Elf
+{
+    public Elf(IEnumerable<int> calorieLines)
+    {
+        _calorieLines = calorieLines;
+    }
+
+    private readonly IEnumerable<int> _calorieLines;
+
+    public void AddCalorieLine(int value)
+    {
+        ((List<int>)_calorieLines).Add(value);
+    }
+
+    public int TotalCalories => _calorieLines.Sum();
+
+    public int NumberOfCalorieLines => _calorieLines.Count();
+}
